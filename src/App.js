e4c8f261a24b1716efd8d42a48ecb5f206460521
Diyor <diyork355@gmail.com>
2024-04-02 08:15:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
 function App() {
+  
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Simple React App with useState</h1>
+      <p>Count: {count}</p>
+      <button onClick={increaseCount}>Increase Count</button>
+      <button className='btn2' onClick={decreaseCount}>Decrease Count</button>
     </div>
   );
 }
